d86fdd57ed827850ab89ab1fdea8488c4e545b88
Michele Kluck <michelenamur27@gmail.com>
2024-05-21 14:52:13 -0300

    add exercicio 4 unidade 1

--- src/exercicios/ex4.java
+++ src/exercicios/ex4.java
@@ -1,5 +1,19 @@
 package exercicios;
 /*Crie um algoritmo em Java que peça ao usuário para que se digite um número inteiro (exemplo: 14). Em seguida, o algoritmo deve mostrar a tabuada do número digitado utilizando o “for”*/
 
+import java.util.Scanner;
 public class ex4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um numero inteiro: ");
+        int num = scanner.nextInt();
+
+        System.out.println("Tabuada de: " + num + ":");
+        for (int i = 1; i <= 10; i++){
+            int resultado = num * i;
+            System.out.println(num + " x " + i + " = " + resultado);
+        }
+        scanner.close();
+    }
 }
