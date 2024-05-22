cc84f9ad81f86fbd829fad075ee72e55d201c452
Michele Kluck <michelenamur27@gmail.com>
2024-05-22 14:50:23 -0300

    exercicio 5 unidade 1

--- src/exercicios/ex4.java
+++ src/exercicios/ex4.java
@@ -3,16 +3,16 @@ package exercicios;
 
 import java.util.Scanner;
 public class ex4 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) { // onde a execução do programa começa.
+        Scanner scanner = new Scanner(System.in); // lê a entrada dp sistema
 
-        System.out.print("Digite um numero inteiro: ");
-        int num = scanner.nextInt();
+        System.out.print("Digite um numero inteiro: "); // exibe uma mensagem ao usuario
+        int num = scanner.nextInt(); // le o numero digitado pelo usuario
 
-        System.out.println("Tabuada de: " + num + ":");
-        for (int i = 1; i <= 10; i++){
-            int resultado = num * i;
-            System.out.println(num + " x " + i + " = " + resultado);
+        System.out.println("Tabuada de: " + num + ":"); // mensagem "tabuada de: "
+        for (int i = 1; i <= 10; i++){ // loop que começa com i igual a 1 e incrementa i em cada 1 iteração até 'i' ser igual a 10
+            int resultado = num * i; // calcula o produto do numero digitado (num) e o valor atual de i
+            System.out.println(num + " x " + i + " = " + resultado); // imprime a multiplicação do numero pelo valor de i e o resultado correspondente
         }
         scanner.close();
     }
---
+++ src/exercicios/ex5.java
@@ -0,0 +1,33 @@
+package exercicios;
+/* Crie um algoritmo em Java que peça ao usuário para que se digite um ano (exemplo: 2032). Em seguida, o algoritmo deve verificar se o ano informado é ou não bissexto*/
+
+import java.util.Scanner;
+
+public class ex5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um ano: "); // exibe mensagem ao usuario
+        int ano = scanner.nextInt(); // le a mensagem do usuario
+
+        boolean bissexto = false; // inicializa uma variavel booleana chamada 'bissexto' como 'false'.essa variavel sera usada para indicar se o ano é bissexto
+        if (ano % 4 == 0) { // verifica se o ano é divisivel por 4. se for, pode ser um ano bissexto, então entramos no bloco if
+            if (ano % 100 == 0) { //vefifica se o ano é divisivel por 100. se for, precisamos de mais uma verificação, pois anos multiplos de 100 so sao bissextos se forem multiplos de 400
+                if (ano % 400 == 0) { // se o ano é divisivel por 400, é um ano bissexto e 'bissexto' é denifinido como 'true'
+                    bissexto = true;
+                } else {
+                    bissexto = false;
+                }
+            } else {
+                bissexto = true;
+            }
+        }
+            if (bissexto) {
+                System.out.println("O ano é bissexto.");
+            } else {
+                System.out.println("O ano não é bissexto.");
+            }
+            scanner.close();
+        }
+    }
+
