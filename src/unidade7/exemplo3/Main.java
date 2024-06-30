4af60a8ad5538c0683d05781b5ccc68474db7579
michelekluck <michelenamur27@gmail.com>
2024-06-30 19:28:56 -0300

    exemplo 3 execeções - try/catch para cada tantativa

--- src/unidade7/exemplo3/Main.java
+++ src/unidade7/exemplo3/Main.java
@@ -1,23 +1,32 @@
 package unidade7.exemplo3;
 
 public class Main {
-    public static void main(String[] args){
-        try {
-            contaCorrente contaExemplo = new contaCorrente(1000);
-            System.out.println("O saldo atual é de R$ " + contaExemplo.getSaldo());
+    public static void main(String[] args) {
+        contaCorrente contaExemplo = new contaCorrente(1000);
+        System.out.println("O saldo atual é de R$ " + contaExemplo.getSaldo());
 
-            // vamos sacar 800
+        // vamos sacar 800
+        try {
             contaExemplo.sacar(800);
             System.out.println("O saldo atual é de R$ " + contaExemplo.getSaldo());
 
+        } catch (saldoInsuficienteException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
             // vamos sacar 200
             contaExemplo.sacar(200);
             System.out.println("O saldo atual é de R$ " + contaExemplo.getSaldo());
+        } catch (saldoInsuficienteException e) {
+            System.out.println(e.getMessage());
+        }
 
+        try {
             // vamos sacar 1000000
             contaExemplo.sacar(1000000);
             System.out.println("O saldo atual é de R$ " + contaExemplo.getSaldo());
-        } catch (saldoInsuficienteException e) {
+        } catch (saldoInsuficienteException e){
             System.out.println(e.getMessage());
         }
     }
